7f935c1cb302dbac9b25631dc735269828b3b7cc
Florent Reshani <florent.reshani@icloud.com>
2021-01-14 14:37:28 +0100

    added login and signup forms and implemented the form dot py and added routes to layout form the pages

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'd97b2c7c3d70e2d7b6fdc60a3d504de7'
 
 posts = [
     {
@@ -24,6 +27,27 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+        
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful please check your username', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
