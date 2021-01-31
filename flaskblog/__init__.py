004118b5721570fcc74a0a5e8ea1689b06cc7f08
Mijada Krasniqi <krasniqi.mijada@gmail.com>
2021-01-31 10:54:59 +0100

    Create, update and delete post, Implement Pagination, Password-Reset-Email
    You need to install:
    Pip install Pillow
    Pip install flask mail
    Create, update and delete post
    Implement Pagination
    Password-Reset-Email

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,11 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+from flask_mail import Mail
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'd97b2c7c3d70e2d7b6fdc60a3d504de7'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 
@@ -14,5 +14,11 @@ bcrypt=Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view='login'
 login_manager.login_message_category='info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError
 from flaskblog.models import User
-from flask_login import current_user
+
 
 
 class RegistrationForm(FlaskForm):
@@ -45,14 +46,37 @@ class UpdateAccountForm(FlaskForm):
     submit = SubmitField('Update')
 
     def validate_username(self, username):
-        if username.data!=current_user:
+        if username.data!=current_user.username:
             user=User.query.filter_by(username=username.data).first()
             if user:
                 raise ValidationError('That username is taken. Please chooise another one ')
 
     def validate_email(self, email):
-        if email.data!=current_user:
-            user=User.query.filter_by(email=email.data).first()
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError('That email is taken. Please chooise another one ')
+                raise ValidationError('That email is taken. Please choose a different one.')
 
+                                      
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
